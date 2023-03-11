2fc1f59148685eb1cfdc88a39c6efa574b1cfa9c
Danel <b.bucsa@gmail.com>
2023-03-11 10:29:57 +0200

    Changes to banner

--- src/common/components/Header/Header.js
+++ src/common/components/Header/Header.js
@@ -1,7 +1,7 @@
 import {menuItems} from "../../../routes";
-import { optiswapUrl, voteBanner, voteSnapshot } from "common/links";
 import Container from "react-bootstrap/Container";
 import NavbarMenu from "../NavbarMenu/NavbarMenu";
+import {optiswapUrl} from "../../links";
 
 import "./Header.scss";
 
@@ -9,11 +9,8 @@ const Header = () => {
 
   return (
     <Container fluid className="header-wrapper">
-      <a href={voteSnapshot} target="_blank" > 
-       <img className="imgBanner" src={voteBanner} alt="banner image" /> </a> 
       <a href={optiswapUrl} className="btn-secondary buy">BUY $HOGE</a>
       <NavbarMenu menu={menuItems} />
-      
     </Container>
   );
 };
