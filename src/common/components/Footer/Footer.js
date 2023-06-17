2635771191abf7ddf40622e19bea4ce693ad9116
zembahk <77575523+zembahk@users.noreply.github.com>
2023-06-16 19:57:05 -0700

    team email

--- src/common/components/Footer/Footer.js
+++ src/common/components/Footer/Footer.js
@@ -7,15 +7,15 @@ import {
   redditUrl, 
   twitterUrl, 
   tiktokUrl,
-  telegramUrl, 
+ // telegramUrl, 
   discordUrl,
-  instagramUrl
+ // instagramUrl
   } from "common/links";
 import Twitter from '../../icons/twitter.png';
-import Telegram from '../../icons/telegram.png';
+//import Telegram from '../../icons/telegram.png';
 import Discord from '../../icons/discord.png';
 import Reddit from '../../icons/reddit.png';
-import Instagram from '../../icons/insta.svg'
+//import Instagram from '../../icons/insta.svg'
 import Tiktok from '../../icons/tiktok.png'
 
 // import { ReactComponent as Insta } from '../../icons/insta.svg';
@@ -31,7 +31,9 @@ const Footer = () => {
       <Container className="footer">
         <Socials />
         <div className="inquiries">
-          For general inquiries, reach us at <a href="mailto:contact@hogefinance.com">contact@hogefinance.com</a><br />
+          For general inquiries, reach us at <a href="mailto:contact@hogefinance.com">contact@hogefinance.com</a>
+          or <a href="mailto:email@hoge.team">email@hoge.team</a> 
+          <br />
           For marketing, reach us at <a href="mailto:marketing@hogefinance.com">marketing@hogefinance.com</a>
         </div>
         <div className="links">
@@ -56,14 +58,14 @@ const Socials = () => {
       </div>
       <div className="links">
         <a href={redditUrl} target="_blank" rel="noreferrer"><img src={Reddit} alt={Reddit} /></a>
-        <a href={telegramUrl} target="_blank" rel="noreferrer"><img src={Telegram} alt={Telegram} /></a>
         <a href={discordUrl} target="_blank" rel="noreferrer"><img src={Discord} alt={Discord} /></a>
         <a href={twitterUrl} target="_blank" rel="noreferrer"><img src={Twitter} alt={Twitter} /></a>
         <a href={tiktokUrl} target="_blank" rel="noreferrer"><img src={Tiktok} alt={Tiktok} /></a>
-        <a href={instagramUrl} target="_blank" rel="noreferrer"><img src={Instagram} alt={Instagram} /></a>
       </div>
     </Container>
   </Container>
 }
+//        <a href={telegramUrl} target="_blank" rel="noreferrer"><img src={Telegram} alt={Telegram} /></a>
+//        <a href={instagramUrl} target="_blank" rel="noreferrer"><img src={Instagram} alt={Instagram} /></a>
 
 export default Footer;
