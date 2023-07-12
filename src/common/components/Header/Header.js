604bf2f82ab1b4af3f1914ff0b28d66898be54e1
zembahk <77575523+zembahk@users.noreply.github.com>
2023-07-12 01:19:05 -0700

    opti update

--- src/common/components/Header/Header.js
+++ src/common/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
 
   return (
     <Container fluid className="header-wrapper">
-      <a href={optiswapUrl} className="btn-secondary buy">BUY $HOGE</a>
+      <a href={optiswapUrl} className="btn-secondary buy">$HOGE @ OptiSwap</a>
       <NavbarMenu menu={menuItems} />
     </Container>
   );
--- src/common/components/NavbarMenu/NavbarMenu.js
+++ src/common/components/NavbarMenu/NavbarMenu.js
@@ -5,7 +5,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink } from 'react-router-dom';
-import Button from '../Button/Button'
 
 import Translate from "../../Translate";
 
@@ -14,7 +13,7 @@ import "./NavbarMenu.scss";
 export const NavbarMenu = (props) => {
 
   return (
-    <Navbar collapseOnSelect expand="md" variant="dark" className="site-navigation">
+    <Navbar collapseOnSelect expand="xl" variant="dark" className="site-navigation">
       <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Brand href="/">
