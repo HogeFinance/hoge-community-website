ca3b64493ece784234dfcf32be7dcd1bc2752ee4
DanelHoge <b.bucsa@yahoo.com>
2023-11-09 17:07:51 +0200

    Print shop prod

--- src/pages/Print/Print.js
+++ src/pages/Print/Print.js
@@ -4,11 +4,17 @@ import useImportScript from "../../hooks/useImportScript";
 
 import { useLayoutEffect } from 'react';
 
-const PARTNER_ID = "3964109273"
-const API_KEY = "AIzaSyC80BSaj3NVR2e8RAHtxNNTmOOd_0OZTvU"
-const INFINITE_SCRIPT = "https://api.sandbox.infiniteobjects.com/print-button";
+//dev
+// const PARTNER_ID = "3964109273"
+// const API_KEY = "AIzaSyC80BSaj3NVR2e8RAHtxNNTmOOd_0OZTvU"
+// const INFINITE_SCRIPT = "https://api.sandbox.infiniteobjects.com/print-button";
 
-export const Print = () => {
+//prod
+const PARTNER_ID = "6906769176"
+const API_KEY = "AIzaSyD6hfkb0tJwxJL5ypy9-06vPUaNJzQfUgs"
+const INFINITE_SCRIPT = "https://api.infiniteobjects.com/print-button";
+
+  export const Print = () => {
 
   useImportScript(`${INFINITE_SCRIPT}/${PARTNER_ID}/client.js`);
 
