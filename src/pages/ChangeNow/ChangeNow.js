d0d5ea11c8ddfa2a387357f57a85ee28405fc6b7
zembahk <77575523+zembahk@users.noreply.github.com>
2024-02-01 06:30:27 +0000

    ChangeNow Update

--- src/pages/ChangeNow/ChangeNow.js
+++ src/pages/ChangeNow/ChangeNow.js
@@ -1,6 +1,6 @@
 import Top from "../../common/components/Top/Top";
 import useImportScript from "../../hooks/useImportScript";
-
+import './ChangeNow.scss'
 export const ChangeNow = () => {
 
   useImportScript('https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js');
@@ -10,14 +10,19 @@ export const ChangeNow = () => {
       <Top
         title="Change Now"
       />
-      <div>
-        <iframe
-          title='changenow'
-          id='iframe-widget'
-          src='https://changenow.io/embeds/exchange-widget/v2/widget.html?FAQ=true&amount=0.01&amountFiat=1500&backgroundColor=1b1b1b&darkMode=true&from=btc&fromFiat=eur&horizontal=false&isFiat&lang=en-US&link_id=291aead12db5b3&locales=true&logo=true&primaryColor=998d32&to=hoge&toFiat=eth&toTheMoon=true'
-          width={"100%"} height={356} style={{border: "none"}}
-        ></iframe>
+      <div className="changenow">  
+      <iframe 
+        id='iframe-widget' 
+        src='https://changenow.io/embeds/exchange-widget/v2/widget.html?FAQ=true&amount=0.3&amountFiat=1500&backgroundColor=2B2B35&darkMode=true&from=eth&fromFiat=usd&horizontal=false&isFiat&lang=en-US&link_id=291aead12db5b3&locales=true&logo=true&primaryColor=998d32&to=hoge&toFiat=hoge&toTheMoon=true' 
+        style={{height: '356px', width: '370px', border: 'none'}}
+      >
+      </iframe>
+      <script 
+        defer type='text/javascript' 
+        src='https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js'>
 
+      </script>
+      
       </div>
     </div>
   )
